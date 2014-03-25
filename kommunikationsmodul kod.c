7e45772d5507edf112bc7ca5bdb00d9dfcb12663
Tore Landén <tore.landen@gmail.com>
2014-03-25 17:18:01 +0100

    indent

--- kommunikationsmodul kod.c
+++ kommunikationsmodul kod.c
@@ -4,19 +4,19 @@
 //Initierar SPI Master
 void SPI_init(void){ 
 
-  DDRB = ((1<<DDB7)|(1<<DDB5)|(1<<DDB4)|(1<<DDB3)); //spi pins on port b MOSI SCK,SS1,SS2 outputs 
+	DDRB = ((1<<DDB7)|(1<<DDB5)|(1<<DDB4)|(1<<DDB3)); //spi pins on port b MOSI SCK,SS1,SS2 outputs 
 
-  SPCR = ((1<<SPE)|(1<<MSTR)|(1<<SPR0));  // SPI enable, Master, f/16 
+	SPCR = ((1<<SPE)|(1<<MSTR)|(1<<SPR0));  // SPI enable, Master, f/16 
 
 } 
 
 //Transmit function. cData på MOSI. Return MISO.
 char SPI_Transmit(char cData){ 
 
-   SPDR = cData; 
-   while(!(SPSR & (1<<SPIF))) 
-      ; 
-   return SPDR; 
+	SPDR = cData; 
+	while(!(SPSR & (1<<SPIF))) 
+		; 
+	return SPDR; 
 }
 
 int main(void)
