0c61c9eaab3c5b855d191b5754e0955c5c5077fc
Jazzandcoffe <herman.molinder@gmail.com>
2014-03-31 15:20:52 +0200

    Fungerande (?) Komm

--- kommunikationsmodul kod.c
+++ kommunikationsmodul kod.c
@@ -1,9 +1,10 @@
 ﻿/*
-* Kommunikationsmodul.c
-*
-* Created: 3/26/2014 1:42:57 PM
-*  Author: hermo276
-*/
+ *  Kommunikationsmodul.c
+ *
+ *  Created: 3/26/2014 1:42:57 PM
+ *  Author:	Herman Molinder		hermo276@student.liu.se
+ *			Tore Landén			torla816@student.liu.se
+ */
 
 #define F_CPU 14745600UL
 
@@ -12,26 +13,24 @@
 #include <util/delay.h>
 
 //Globala variabler
-volatile int init_transmit; //För att hålla reda på när vi ska använda buss.
-volatile char transmit_buffer; //Data som ska skickas
-volatile char recieve_buffer; //Data som tas emot
-volatile int auto_or_manual; //autonomt läge = 0/manuellt läge = 1
-volatile char type_sens; //typ-byte till protokollet
-volatile char data_sens; //data-byte till protokollet
-volatile char check_sens; //check-byte till protokollet
-volatile char type_styr; //typ-byte till protokollet
-volatile char data_styr; //data-byte till protokollet
-volatile char check_styr; //ckeck-byte till protokollet
+volatile int	init_transmit;	//För att hålla reda på när vi ska använda buss.
+volatile int	auto_or_manual; //autonomt läge = 0/manuellt läge = 1
+volatile char	recieve_buffer; //Data som tas emot
+volatile char	type_sens;		//typ-byte till protokollet
+volatile char	data_sens;		//data-byte till protokollet
+volatile char	check_sens;		//check-byte till protokollet
+volatile char	type_styr;		//typ-byte till protokollet
+volatile char	data_styr;		//data-byte till protokollet
+volatile char	check_styr;		//ckeck-byte till protokollet
 
 
 //Initierar SPI Master
 void SPI_init(void)
 {
 	//spi pins on port b MOSI SCK,SS1,SS2 outputs
-	DDRB = ((1<<DDB7)|(1<<DDB5)|(1<<DDB4)|(1<<DDB3)); 
-	// SPI enable, Master, f/16
-	SPCR = ((1<<SPE)|(1<<MSTR)|(1<<SPR0));  
-
+	DDRB = ((1<<DDB7)|(1<<DDB5)|(1<<DDB4)|(1<<DDB3));
+	// SPI enable, Master, f/4
+	SPCR = ((1<<SPE)|(1<<MSTR));
 }
 
 //Transmit function. cData på MOSI. Return MISO.
@@ -64,25 +63,18 @@ ISR(TIMER1_OVF_vect)
 
 ISR(INT0_vect)
 {
-	if(auto_or_manual == 0)
-	{
-		auto_or_manual = 1;
-	}
-	else
-	{
-		auto_or_manual = 0;
-	}
+	auto_or_manual = auto_or_manual^0x0001;
 	PORTB = (0<<PB4);
 	SPI_transmit(0x00);
 	PORTB = (1<<PB4);
 }
 
-/* 
-SS för sensor, vi skickar aldrig, 
-så denna funktion läser vad som 
-sensor lagt på bussen
+/*
+* SS för sensor, vi skickar aldrig,
+* så denna funktion läser vad som
+* sensor lagt på bussen
 */
-char ss_sensor() 	
+char ss_sensor()
 {
 	PORTB = (0 << PB3);
 	recieve_buffer = SPI_transmit(0x00);
@@ -91,8 +83,8 @@ char ss_sensor()
 }
 
 /*
-SS för styr, vi skickar sensordata,
-tar emot styrbeslut.(som ska skickas via BT)
+* SS för styr, vi skickar sensordata,
+* tar emot styrbeslut.(som ska skickas via BT)
 */
 char ss_styr(char to_send)
 {
@@ -102,28 +94,36 @@ char ss_styr(char to_send)
 	return recieve_buffer;
 }
 
+// check_decoder returnerar 1 om check == type XOR data, annars 0.
+unsigned int check_decoder(char type, char data, char check)
+{
+	char is_check = type^data;
+	return (check == is_check);
+}
+
+// Returnerar en "checksum" = type XOR data.
+char check_creator(char type,char data)
+{
+	return type^data;
+}
+
 int main(void)
 {
 
 	SPI_init();
 	timer1_init();
 	// set Global Interrupt Enable
-	sei(); 
+	sei();
 	auto_or_manual = 1;
-	
-	//testkod
-	type_styr = 0x08;
-	data_styr = 0x0F;
-	check_styr = 0xFF;
-	//testkod
 
 	// loop forever
 	for (;;)
 	{
-		//Autonomt läge
-		if(auto_or_manual == 0)
+		
+		if (init_transmit==1)
 		{
-			if(init_transmit==1)
+			//Autonomt läge
+			if(auto_or_manual == 0)
 			{
 				for (int i=0;i<10;i++)
 				{
@@ -132,42 +132,40 @@ int main(void)
 					data_sens = ss_sensor();
 					_delay_us(10);
 					check_sens = ss_sensor();
-					/*
-					Här ska det vara kod för ckeck
-					*/
-					//if(check_ok)
-					type_styr = ss_styr(type_sens);
-					_delay_us(10);
-					data_styr = ss_sensor(data_sens);
-					_delay_us(10);
-					check_styr = ss_sensor(check_sens);
-					/*
-					Här ska det vara kod för check
-					*/
-					//if(ckeck_ok)
-					/*
-					Här ska det vara kod för bluetoothsändning 
-					av styrbeslut och sensorvärden
-					*/
+					
+					if(check_decoder(type_sens, data_sens, check_sens))
+					{
+						type_styr = ss_styr(type_sens);
+						_delay_us(10);
+						data_styr = ss_sensor(data_sens);
+						_delay_us(10);
+						check_styr = ss_sensor(check_sens);
+						/*
+						Här ska det vara kod för bluetoothsändning
+						av sensorvärden
+						*/
+					}
+					if(check_decoder(type_styr, data_styr, check_styr))
+					{
+						/*
+						 * Här kod för BT sändning av styrbeslut
+						 */
+					}
+					init_transmit = 0;
 				}
-				
-				init_transmit = 0;
 			}
-		}
-		//Manuellt läge
-		else 
-		{
-			if(init_transmit==1)
+			//Manuellt läge
+			else
 			{
 				/*
-				Här ska det vara kod för bluetoothsändning 
+				Här ska det vara kod för bluetoothsändning
 				av styrbeslut och sensorvärden
 				*/
-				type_sens = ss_styr(type_styr);
+				type_styr = ss_styr(type_styr);
 				_delay_us(10);
-				data_sens = ss_styr(data_styr);
+				data_styr = ss_styr(data_styr);
 				_delay_us(10);
-				check_sens = ss_styr(check_styr);
+				check_styr = ss_styr(check_creator(type_styr, data_styr));
 				init_transmit = 0;
 			}
 		}
